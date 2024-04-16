4f4080ca7b817c471fecaeca16674a1c149b701b
xujian <xujian@dip-ai.com>
2024-04-16 13:41:56 +0800

    初始化项目

---
+++ main.js
@@ -0,0 +1,7 @@
+// require('dotenv').config()
+// console.log(process.env)
+const advantage = require('./src/advantage')
+
+module.exports = {
+  advantage,
+}
---
+++ src/advantage.js
@@ -0,0 +1,8 @@
+const Decimal = require('decimal.js')
+// add sub mul div
+
+function advantage (data) {
+  return data
+}
+
+module.exports = advantage
